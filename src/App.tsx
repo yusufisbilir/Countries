73751de0008b71bbc6a3181ee3276474d6c01788
Yusuf Isbilir <dev.yusufisbilir@gmail.com>
2022-01-20 19:41:38 +0300

    Fetch data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { CountryType } from "./types";
+
 function App() {
-  return <div>Countries app with TypeScript</div>;
+  const [countries, setCountries] = useState<CountryType[]>([]);
+
+  const getCountries = async () => {
+    try {
+      const { data } = await axios.get<CountryType[]>(
+        "https://restcountries.com/v2/all"
+      );
+      setCountries(data);
+    } catch {
+      console.log("Country api error!");
+    }
+  };
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  console.log(countries);
+  return (
+    <div>
+      Countries app with TypeScript
+      {countries.map((country) => (
+        <h1 key={country.numericCode}>{country.name}</h1>
+      ))}
+    </div>
+  );
 }
 
 export default App;
